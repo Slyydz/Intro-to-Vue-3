e02c49065b7d8998b29aa82d19356e4bad39389c
Brady Williams <williams.cole29@yahoo.com>
2022-02-25 14:45:02 -0600

    Finished Lesson 5

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2022, color: 'green'},
+                {id: 2023, color: 'blue'}
+            ],
+            sizes: ['small', 'medium', 'large']
         }
     }
 })
